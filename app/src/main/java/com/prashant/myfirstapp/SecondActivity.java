f698213fc9cd67868806d268ee6a70b41f9468ad
Prashant Nag <prashantnag.workmail@gmail.com>
2015-10-15 03:37:52 +0530

    Launching Second Activity from Button.

---
+++ app/src/main/java/com/prashant/myfirstapp/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.prashant.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by prashant on 15/10/15.
+ */
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
